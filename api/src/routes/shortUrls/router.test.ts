077625a56b5858887ac0bd6cb9d5bb3e0ba2cbaf
harrik123 <harri.kuittinen@gmail.com>
2021-02-25 22:17:01 +0200

    Add create short url test

--- api/src/routes/shortUrls/router.test.ts
+++ api/src/routes/shortUrls/router.test.ts
@@ -1,7 +1,10 @@
-import app from "../../app";
+import app, { API_URL, STORAGE_PATH } from "../../app";
 import supertest from "supertest";
 import * as utils from "../../utils/functions";
 import { IShortUrlItem } from "../../ts/interfaces";
+import fsPromises from "fs/promises";
+import nanoid from "nanoid";
+import { V1_SHORT_URLS } from "./router";
 
 const request = supertest(app);
 const mockItems: IShortUrlItem[] = [
@@ -54,5 +57,34 @@ test("GET original url expired", async (done) => {
   expect(res.status).toBe(200);
   expect(res.body.data).toBe(undefined);
   expect(res.body.message).toBe("Shortened url is expired.");
+  done();
+});
+
+test("POST create short url success", async (done) => {
+  const storedItems: IShortUrlItem[] = [];
+  jest
+    .spyOn(utils, "getStoredItems")
+    .mockReturnValueOnce(Promise.resolve(storedItems));
+
+  const newItemKey = "123";
+  const mockNanoid = jest.fn().mockReturnValue(newItemKey);
+  nanoid.nanoid = mockNanoid;
+
+  const mockWriteFile = jest.fn();
+  fsPromises.writeFile = mockWriteFile;
+
+  const res = await request
+    .post("/v1/short-urls")
+    .send({ url: "https://created.com" });
+
+  expect(mockWriteFile).toHaveBeenCalledWith(
+    STORAGE_PATH,
+    JSON.stringify(storedItems)
+  );
+  expect(res.status).toBe(200);
+  expect(res.body.data.shortUrl).toBe(
+    `${API_URL}${V1_SHORT_URLS}/${newItemKey}`
+  );
+  expect(storedItems[0].key).toBe(newItemKey);
   done();
 });
