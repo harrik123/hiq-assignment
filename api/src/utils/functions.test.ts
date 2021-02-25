26477f7c24a07745bc18154d15df6023aad928cb
harrik123 <harri.kuittinen@gmail.com>
2021-02-25 21:34:27 +0200

    Add util test

--- api/src/utils/functions.test.ts
+++ api/src/utils/functions.test.ts
@@ -1,5 +1,44 @@
+import fsPromises from "fs/promises";
+import { STORAGE_PATH } from "../app";
 import { IShortUrlItem } from "../ts/interfaces";
-import { isShortUrlStillValid } from "./functions";
+import { getStoredItems, isShortUrlStillValid } from "./functions";
+
+test("Get stored items and file existst", async (done) => {
+  const dataFromStorage: IShortUrlItem[] = [
+    {
+      key: "123",
+      url: "https://asd.com",
+      created: new Date().toISOString(),
+    },
+  ];
+
+  const mockReadFile = jest
+    .fn()
+    .mockResolvedValue(JSON.stringify(dataFromStorage));
+
+  fsPromises.readFile = mockReadFile;
+
+  const result = await getStoredItems();
+
+  expect(result).toMatchObject(dataFromStorage);
+  expect(mockReadFile).toHaveBeenCalledWith(STORAGE_PATH, "utf8");
+
+  done();
+});
+
+test("Get stored items and file doesn't exist", async (done) => {
+  const mockReadFile = jest.fn().mockImplementation(() => {
+    throw { code: "ENOENT" };
+  });
+
+  fsPromises.readFile = mockReadFile;
+
+  const result = await getStoredItems();
+
+  expect(result).toHaveLength(0);
+
+  done();
+});
 
 test("Url created over a week ago", () => {
   testUrlIsValid(8, false);
